9c8c423b0c906376ee651346320e0d160b5ab3b3
Milda Barskiete <stanislovaityte.m@gmail.com>
2023-09-01 10:12:39 +0300

    localStorage

--- habit-tracking-app/src/components/main_content/store.js
+++ habit-tracking-app/src/components/main_content/store.js
@@ -2,36 +2,41 @@ import { defineStore } from 'pinia'
 
 export const useAppStore = defineStore('app', {
     state: () => ({
-        habits: [],
-        categories: [],
-        selectedDay: null,
-        selectedCategories: [],
-        habitsByDay: {},
+        habits: JSON.parse(localStorage.getItem('habits')) || [],
+        categories: JSON.parse(localStorage.getItem('categories')) || [],
+        selectedDay: JSON.parse(localStorage.getItem('selectedDay')) || null,
+        selectedCategories: JSON.parse(localStorage.getItem('selectedCategories')) || [],
+        habitsByDay: JSON.parse(localStorage.getItem('habitsByDay')) || {},
     }),
     actions: {
         addHabit({ habit, category }) {
             this.habits.push({ habit, category })
+            localStorage.setItem('habits', JSON.stringify(this.habits));
         },
         addCategory(category) {
             this.categories.push(category)
+            localStorage.setItem('categories', JSON.stringify(this.categories))
         },
         setSelectedDay(day) {
-            console.log('Updated selected Day: ', day);
             this.selectedDay = day
+            localStorage.setItem('selectedDay', JSON.stringify(this.selectedDay))
         },
         addHabitToDay({ day, habit, category }) {
             if (!this.habitsByDay[day]) {
                 this.habitsByDay[day] = []
             }
             this.habitsByDay[day].push({ habit, category })
+            localStorage.setItem('habitsByDay', JSON.stringify(this.habitsByDay))
         },
         setSelectedCategories(categories) {
             this.selectedCategories = categories;
+            localStorage.setItem('selectedCategories', JSON.stringify(this.selectedCategories))
         },
         removeCategory(category) {
             const index = this.categories.indexOf(category);
             if (index !== -1) {
                 this.categories.splice(index, 1);
+                localStorage.setItem('categories', JSON.stringify(this.categories))
             }
         }
     },
@@ -57,5 +62,6 @@ export const useAppStore = defineStore('app', {
         getAllCategories() {
             return this.categories;
         },
-    }
+    },
+    persist: true,
 })
--- habit-tracking-app/src/main.js
+++ habit-tracking-app/src/main.js
@@ -2,10 +2,12 @@ import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router.js'
-import "bootstrap/dist/css/bootstrap.min.css";
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 
 const pinia = createPinia();
 const app = createApp(App)
+
+pinia.use(piniaPluginPersistedstate)
 
 app.use(router).use(pinia).mount('#app')
 
